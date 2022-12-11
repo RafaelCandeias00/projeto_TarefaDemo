7b96cd18b1d037bff590a2a892ac673e5b59f393
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-12-11 19:17:41 -0300

    @RafaelCandeias00 [Task: add métodos POST-PUT-DELETE]

--- TarefaDemo/TarefaDemo/Endpoints/TarefasEndpoints.cs
+++ TarefaDemo/TarefaDemo/Endpoints/TarefasEndpoints.cs
@@ -20,6 +20,45 @@ namespace TarefaDemo.Endpoints
                 }
                 return Results.Ok(tarefas);
             });
+
+            app.MapGet("/tarefas/{id:int}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+                /*var tarefa = con.Get<Tarefa>(id);
+                if (tarefa is null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(tarefa);*/
+
+                return con.Get<Tarefa>(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound();
+            });
+
+            app.MapPost("/tarefas", async (GetConnection connectionGetter, Tarefa tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Insert(tarefa);
+                return Results.Created($"/tarefas/{id}", tarefa);
+            });
+
+            app.MapPut("/tarefas", async (GetConnection connectionGetter, Tarefa tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Update(tarefa);
+                return Results.Ok();
+            });
+
+            app.MapDelete("/tarefas/{id:int}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+                var deletado = con.Get<Tarefa>(id);
+                if (deletado is null)
+                {
+                    return Results.NotFound();
+                }
+                con.Delete(deletado);
+                return Results.Ok(deletado);
+            });
         }
     }
 }
